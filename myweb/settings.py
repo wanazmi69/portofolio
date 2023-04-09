852853c14acac917bf9e88935bb98f0dc02db122
wanazmi <lawtheoscar@gmail.com>
2023-04-09 13:12:07 +0900

    up to vercel

--- myweb/settings.py
+++ myweb/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 else:
-    ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
+    ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = [
 #     'localhost',
 #     '127.0.0.1'
